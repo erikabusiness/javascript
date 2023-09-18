b8a30c437246ca20a44010657134ca68103e0fd2
Erika Alves <erikaalves.m.silva@gmail.com>
2023-09-18 18:09:50 -0300

    Exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+let num = 4
+
+if (num % 2 == 0) {
+    console.log("O numero é par")
+} else {
+    console.log ("o número não é par")
+}
+
+console.log(`O numero é ${num % 2 == 0 ? 'par' : 'impar'}.`) /** IF TERNARIO */
